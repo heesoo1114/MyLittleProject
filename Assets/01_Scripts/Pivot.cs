f9ad5beea17ad3b00948ed518cc7c13a4490ebfd
Yoon HeeSoo <improveme7@gmail.com>
2023-02-27 02:51:23 +0900

    delete and start

--- Assets/01_Scripts/Pivot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Pivot : MonoBehaviour
-{
-    public void Roatate()
-    {
-        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z + 60);
-    }
-}
